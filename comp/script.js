abe7fe0bb3997790fd087ef516754485997e3cf0
DarthSalad <piyushmishra965@gmail.com>
2021-05-29 19:01:14 +0530

    almost there on dropdown

---
+++ comp/script.js
@@ -0,0 +1,21 @@
+const F = document.getElementsByClassName("sect")
+const H = document.getElementsByClassName("faqhead")
+const A = document.getElementsByClassName("arrow")
+
+for(var i=0; i<Q.length; ++i) {
+    H[i].addEventListener("click", toggleItem, false);
+}
+
+function toggleItem() {
+    var S = this.parentNode.className;
+    for(var i=0; i<F.length; ++i){
+        F[i].className = "sect";
+        H[i].className = "faqhead";
+        A[i].className = "arrow";
+    }
+    if(S == "sect"){
+        this.parentNode.classname = "sect sect-clicked";
+        H.classname = "faqhead faqhead-clicked";
+        this.firstElementChild.className = "arrow arrow-clicked";
+    }
+}
