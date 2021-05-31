abc446a13af193a350cf2481d23b7280fced3a77
DarthSalad <piyushmishra965@gmail.com>
2021-05-31 12:13:13 +0530

    updated js

--- comp/script.js
+++ comp/script.js
@@ -2,17 +2,17 @@ const F = document.getElementsByClassName("sect")
 const H = document.getElementsByClassName("faqhead")
 const A = document.getElementsByClassName("arrow")
 
-for(var i=0; i<Q.length; ++i) {
-    H[i].addEventListener("click", toggleItem, false);
+for(var i=0; i<F.length; ++i) {
+    F[i].addEventListener("click", toggleItem, false);
 }
 
 function toggleItem() {
     var S = this.parentNode.className;
-    for(var i=0; i<F.length; ++i){
-        F[i].className = "sect";
-        H[i].className = "faqhead";
-        A[i].className = "arrow";
-    }
+    // for(var i=0; i<F.length; ++i){
+    //     F[i].className = "sect";
+    //     H[i].className = "faqhead";
+    //     A[i].className = "arrow";
+    // }
     if(S == "sect"){
         this.parentNode.classname = "sect sect-clicked";
         H.classname = "faqhead faqhead-clicked";
